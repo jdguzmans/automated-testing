47952c542419839c7b60812327770cfdb813fdd7
julieasf7 <ja.sarmiento@uniandes.edu.co>
2018-08-25 02:11:57 -0500

    Codigo base para las pruebas E2E

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ program
     .option('-e2e, --end-to-end', 'end to end tests')
 
     .action((path) => {
-      console.log(path)
+        var exec = require('child_process').exec, child;
+
+      	child = exec('sh index.sh',
+	// Pasamos los parámetros error, stdout la salida 
+	// que mostrara el comando
+	function (error, stdout, stderr) {
+	    // Imprimimos en pantalla con console.log
+	    console.log(stdout);
+	    // controlamos el error
+	    if (error !== null) {
+	      console.log('exec error: ' + error);
+	    }
+	});
     })
     .parse(process.argv)
